042aa44a598de7a3934d0530ae5153dc651f39c7
phxtho <phathopukwana@gmail.com>
2024-12-10 16:59:42 +0100

    update uniforms based on user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::f32::consts::PI;
 use std::path::PathBuf;
 
 use anyhow::{Error, Ok};
@@ -16,12 +17,14 @@ mod graphics;
 #[derive(Default)]
 struct App {
     graphics: Option<Graphics>,
+    uniforms: [f32; 6],
 }
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         let window = event_loop.create_window(Default::default()).unwrap();
         let image_volume = load_image_volume("data/eclipse-10.0.42-fsrt-brain").unwrap();
+        self.uniforms = [0.01, 1., 1., 1., 1., 0.];
         self.graphics = Some(Graphics::new(window, image_volume).block_on());
     }
 
@@ -46,13 +49,42 @@ impl ApplicationHandler for App {
             }
             WindowEvent::RedrawRequested => {
                 let graphics = self.graphics.as_mut().unwrap();
-                let sliders = vec![1.; 6];
-                graphics.render(sliders.as_slice()).unwrap();
+                graphics.render(self.uniforms.as_slice()).unwrap();
                 graphics.window.request_redraw();
+            }
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        physical_key:
+                            key @ (PhysicalKey::Code(KeyCode::ArrowLeft)
+                            | PhysicalKey::Code(KeyCode::ArrowRight)
+                            | PhysicalKey::Code(KeyCode::ArrowUp)
+                            | PhysicalKey::Code(KeyCode::ArrowDown)),
+                        state: ElementState::Pressed,
+                        ..
+                    },
+                ..
+            } => {
+                handle_user_input(key, &mut self.uniforms);
             }
             _ => (),
         }
     }
+}
+
+fn handle_user_input(key: PhysicalKey, uniforms: &mut [f32; 6]) {
+    let slice_delta: f32 = 0.05;
+    let rotation_delta = 0.1;
+
+    match key {
+        PhysicalKey::Code(KeyCode::ArrowLeft) => uniforms[5] += rotation_delta,
+        PhysicalKey::Code(KeyCode::ArrowRight) => uniforms[5] -= rotation_delta,
+        PhysicalKey::Code(KeyCode::ArrowUp) => uniforms[0] += slice_delta,
+        PhysicalKey::Code(KeyCode::ArrowDown) => uniforms[0] -= slice_delta,
+        _ => (),
+    }
+    uniforms[5] = uniforms[5].clamp(-PI, PI);
+    uniforms[0] = uniforms[0].clamp(0., 1.);
 }
 
 fn load_image_volume(path: &str) -> Result<ImageVolume, Error> {
@@ -72,7 +104,7 @@ fn main() -> Result<(), anyhow::Error> {
 
 async fn run() {
     let event_loop = EventLoop::new().expect("Failed to create event loop");
-    event_loop.set_control_flow(ControlFlow::Poll); // Assuming this is better for real-time rendering
+    event_loop.set_control_flow(ControlFlow::Poll);
     let mut app = App::default();
     event_loop.run_app(&mut app).unwrap();
 }
